d16676de1aa6d3b565c14924eacdfa71bf555b24
yamamoto <yoyoabab762@gmail.com>
2020-06-09 09:28:22 +0900

    game making

---
+++ gameDay2/Assets/NextStageController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextStageController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Player"));
+        {
+            SceneManager.LoadScene("CompStage02");
+        }
+    }
+}
